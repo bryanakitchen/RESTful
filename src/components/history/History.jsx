df400ef3c04a13ddcee292a480a3b7122537a672
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-04 18:42:59 -0500

    add react-json-view

--- src/components/history/History.jsx
+++ src/components/history/History.jsx
@@ -5,6 +5,12 @@ const History = () => {
   return (
     <>
       <p>Hello from History</p>
+
+      <article>
+        <h2>History</h2>
+        {/* list goes here */}
+      </article>
+
       <HistoryItem />
     </>
   );
--- src/components/history/HistoryItem.jsx
+++ src/components/history/HistoryItem.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const HistoryItem = () => {
+const HistoryItem = ({ method, url, id }) => {
   return (
-    <div>
-      Hello from HistoryItem
-    </div>
+    <article id={id}>
+      <h3 id={id}>{method}</h3>
+      <p id={id}>{url}</p>
+    </article>
+
   );
+};
+
+HistoryItem.propTypes = {
+  method: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired
 };
 
 export default HistoryItem;
