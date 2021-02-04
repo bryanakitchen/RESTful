5f0a15bbc8e1f341b5be3ab2a0bcb295c5b5f763
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-04 18:56:13 -0500

    update history component

--- src/components/history/History.jsx
+++ src/components/history/History.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import HistoryItem from './HistoryItem';
+import PropTypes from 'prop-types';
 
 const History = () => {
+  // const historyElements = history.map((item, i) => {
+  //   return (
+  //     <li key={i}>
+  //       <HistoryItem history={history} />
+  //     </li>
+  //   );
+  // });
+
   return (
     <>
-      <p>Hello from History</p>
-
       <article>
         <h2>History</h2>
-        {/* list goes here */}
+        <ul>
+          {/* {historyElements} */}
+        </ul>
       </article>
-
-      <HistoryItem />
     </>
   );
+};
+
+History.propTypes = {
+  history: PropTypes.arrayOf(PropTypes.shape({
+    method: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired
+  })).isRequired
 };
 
 export default History;
