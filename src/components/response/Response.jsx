6d551ba35585b672600ede9d58175f8f0cd8a54d
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-04 18:21:13 -0500

    add react-json-view

--- src/components/response/Response.jsx
+++ src/components/response/Response.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import ReactJson from 'react-json-view';
 
 const Response = () => {
   return (
     <div>
-      Hello from Response.
+      <ReactJson />
     </div>
   );
 };
