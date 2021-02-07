235eb94aca3a1b407edaee626908d5bb8fa77bba
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-07 13:15:23 -0500

    add scroll and max-vh to prevent ridiculous scrolling of entire app

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-//Need to add fonts
 const Banner = styled.p`
   text-align: center;
   color: #FEFCFB;
--- src/globalStyles.js
+++ src/globalStyles.js
@@ -3,30 +3,18 @@ import { createGlobalStyle } from 'styled-components';
  
 const GlobalStyle = createGlobalStyle`
 
-body {
-    width: 100%;
-    height: 100%;
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-    background: #001F54;
-  }
+  body {
+      width: 100%;
+      height: 100vh;
+      padding: 0;
+      margin: 0;
+      box-sizing: border-box;
+      background: #001F54;
+    }
 
   html {
     font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
   }
-  `;
+`;
   
 export default GlobalStyle;
-
-// #0A1128   #001F54   #034078    #1282A2   #FEFCFB
-
-// border: solid 10px yellow;
-// font-family: Open-Sans, Helvetica, Sans-Serif;
-// flex-direction: row;
-// flex-wrap: wrap;
-// justify-content: center;
-// align-items: center;
-// width: auto;
-// flex-direction: row;
-// display: flex;
